6dc456692a66f2c031df1ea880f6c21d1dd2665a
Louis Moraes <lmoraes@hotmail.fr>
2021-10-21 17:48:27 +0200

    :tada: Bases du projet

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    /* APP */
+    define('APP_NAME', 'Quiz Millennials');
+    define('CREDITS', 'Par Louis Moraes');
+
+    /* DATABASE */
+    define('DB_DSN', 'mysql:host=localhost;dbname=quizmillennials;charset=utf8');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+?>
