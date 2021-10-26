63805434b759dc601a727203a3ae5afd7c2b906b
Louis Moraes <lmoraes@hotmail.fr>
2021-10-26 21:18:05 +0200

    :wrench: Maj de config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-    /* APP */
-    define('APP_NAME', 'Quiz Millennials');
-    define('CREDITS', 'Par Louis Moraes');
+/* APP */
+define('APP_NAME', 'Quiz Millennials');
+define('CREDITS', 'Par Louis Moraes');
+define('NB_QUESTIONS', 12);
 
-    /* DATABASE */
-    define('DB_DSN', 'mysql:host=localhost;dbname=quizmillennials;charset=utf8');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-?>
+/* DATABASE */
+define('DB_DSN', 'mysql:host=localhost;dbname=quiz_millennials;charset=utf8');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
